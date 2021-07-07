8703852ccd34ec6fd4aa2c6e5a1961593439156e
Edward <e.arechwa@gmail.com>
2021-07-07 21:44:10 +0200

    import bootstrap and components

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// components
+import components from "@/components/UI";
+
+const app = createApp(App);
+components.forEach((component) => {
+  app.component(component.name, component);
+});
+app.use(store);
+app.use(router);
+app.mount("#app");
