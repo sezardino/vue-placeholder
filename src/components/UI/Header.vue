3e25353feb61c01cfa5ea6b6f4d6374345e68a38
Edward <e.arechwa@gmail.com>
2021-07-07 21:43:47 +0200

    add ui library

---
+++ src/components/UI/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Vue Placeholder</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link to="/" active-class="active" class="nav-link"
+            >Home</router-link
+          >
+          <router-link to="/posts" active-class="active" class="nav-link"
+            >Posts</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "my-header",
+};
+</script>
---
+++ src/components/UI/index.js
@@ -0,0 +1,5 @@
+import Header from "./Header";
+
+const components = [Header];
+
+export default components;
