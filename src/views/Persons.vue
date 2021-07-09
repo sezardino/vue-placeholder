d328f4c077cb5486a85c622d6cec927291b4d54f
Edward <edward.arechwa@gogomedia.pl>
2021-07-09 15:46:05 +0200

    feat: persons page

--- src/views/Persons.vue
+++ src/views/Persons.vue
@@ -1,9 +1,36 @@
 <template>
-  <my-section title="Persons" class="persons">
-    <h1>This is persons page</h1>
+  <my-spinner v-if="loading" />
+  <my-section v-else title="Persons" class="persons">
+    <h1>Users</h1>
+    <my-row>
+      <my-col col="4" class="persons__filters">
+        <my-filters></my-filters>
+      </my-col>
+    </my-row>
+    <my-users-list :users="users" class="persons__list"></my-users-list>
   </my-section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { loading: true, users: [] };
+  },
+  async mounted() {
+    this.users = await this.$store.dispatch("users/getUsers");
+    this.loading = false;
+  },
+};
 </script>
+
+<style lang="scss">
+.persons {
+  &__filters {
+    margin-left: auto;
+  }
+
+  &__list {
+    margin-top: 40px;
+  }
+}
+</style>
