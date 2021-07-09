7dd0f50269d7789fffe081c2811856ef70a8c550
Edward <edward.arechwa@gogomedia.pl>
2021-07-09 15:45:39 +0200

    change: img url

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="@/assets/logo.png" />
     <HelloWorld msg="Welcome to Vue Placeholder App" />
   </div>
 </template>
